5c98413cfa3f4eaa4b1ef64466ec3e344f4fbee0
Suzanne Cabral <suzannecabral@live.com>
2020-10-26 19:14:23 -0700

    delete works

--- index.js
+++ index.js
@@ -9,28 +9,17 @@ app.use(express.json());
 const PORT = 2222;
 
 //make some data
-const users = [{
+let users = [{
     id: generate(), // hint: use the shortid npm package to generate it
     name: "Jane Doe", // String, required
     bio: "Not Tarzan's Wife, another Jane",  // String, required
+  },{
+    id:generate(),
+    name:"Jasmine",
+    bio:"Tiger Princess"
   },
 ];
 
-
-
-
-
-
-
-
-// GET: /api/users/:id
-// Returns the user object with the specified id.
-//------------------------
-
-
-// DELETE: /api/users/:id
-// Removes the user with the specified id and returns the deleted user.
-//------------------------
 
 
 // PUT: /api/users/:id
@@ -39,6 +28,33 @@ const users = [{
 //------------------------
 
 
+// DELETE: /api/users/:id
+// Removes the user with the specified id and returns the deleted user.
+//------------------------
+
+//err: 404 user with id not found
+//err: ?500 server error removing user from db
+app.delete('/api/users/:id', (req,res)=>{
+
+    const { id } = req.params;
+    const foundUser = users.find(user => user.id === id);
+
+    if(!foundUser){
+        res.status(400).json({
+            message:`No user found with id ${id}`
+        });
+    }else{
+        users = users.filter(user => user.id !== id);
+        res.status(200).json(foundUser);
+    }
+//not sure how to add a server error clause
+//look this up
+
+
+//fix: "Cannot DELETE /api/users/wJCr24HFs"
+//doesn't display my error message
+//id matches an existing id
+});
 
 // POST: /api/users
 // Creates a user using the information sent inside the request body.
@@ -64,6 +80,23 @@ app.post('/api/users', (req,res) => {
         //add to db
         users.push(newUser);
         res.status(201).json(newUser);
+    }
+});
+
+// GET: /api/users/:id
+// Returns the user object with the specified id.
+//------------------------
+app.get('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    const foundUser = users.find(user => user.id === id);
+    
+    //if user id is not found:
+    if (!foundUser){
+        //return relevant error
+        res.status(404).json({message:`No user found with id: ${id}`});
+    }else{
+        //send status code and data
+        res.status(200).json(foundUser);
     }
 });
 
